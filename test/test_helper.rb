6fedde88d1e2dc896bf9088da56959cd60ba994b
ClaireBvs <clairembeauvais@gmail.com>
2018-06-11 13:11:44 -0600

    implementing features for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,10 @@ class CapybaraTestCase < Minitest::Test
   include Capybara::DSL
   include Capybara::Minitest::Assertions
 end
+
+# If you are not using Rails, define a base class for your Capybara tests like so:
+
+# def teardown
+#     Capybara.reset_sessions!
+#     Capybara.use_default_driver
+# end
