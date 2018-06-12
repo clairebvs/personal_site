19c63485ccaaa09a9370ba033a3a7abd0c4bf0a8
ClaireBvs <clairembeauvais@gmail.com>
2018-06-12 09:01:29 -0600

    implementing test for error page

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,8 +4,14 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
+  end
+
+  def test_user_try_to_see_home_page
+    visit 'error'
+
+    assert_equal 404, page.status_code
+    # assert_equal "Does not exist", page.status_code
   end
 end
